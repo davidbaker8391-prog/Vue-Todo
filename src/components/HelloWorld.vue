2053c83962b3885bd222908f674785b96bc302c3
greendev8391 <green.dev8391@gmail.com>
2020-10-30 04:46:25 +0100

    Add Vuetify and click handler

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,26 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    Test Todo
+    <h2>{{ testing }}</h2>
+    <TodoForm />
   </div>
 </template>
 
 <script>
+import TodoForm from './TodoForm/TodoForm';
+
 export default {
   name: 'HelloWorld',
+  components: {
+    TodoForm,
+  },
+  computed: {
+    testing() {
+      return 'This value is computed';
+    },
+  },
   props: {
     msg: String,
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
---
+++ src/components/TodoForm/TodoForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="todoform">
+    <h1>This text comes from TodoForm</h1>
+    <h2>{{ testText }}</h2>
+    <button @click="test">Button</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoForm',
+  methods: {
+    test() {
+      // return 'This is test value from method in TodoForm';
+      this.testText = 'Endret tekst fra test';
+    },
+  },
+  data() {
+    return {
+      testText: 'Teeest',
+    };
+  },
+};
+</script>
