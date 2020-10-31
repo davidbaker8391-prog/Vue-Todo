053d3a89a3341caa10df312a627fbb44d4e1dace
greendev8391 <green.dev8391@gmail.com>
2020-10-31 03:40:20 +0100

    Add test TODO by clicking button

--- src/components/TodoForm/TodoForm.vue
+++ src/components/TodoForm/TodoForm.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="container p-4 mx-auto">
-    <h1>This text comes from TodoForm.vue</h1>
+    <h1>TODO</h1>
     <main>
       <div class="mt-3">
         <div class="grid grid-cols-12 gap-4">
           <div class="col-span-6 px-1 space-y-4 overflow-y-auto">
-            Content
+            <div
+              v-for="(todo, index) in state.todos"
+              :key="index"
+              class="flex items-center justify-between p-8 bg-white rounded shadow-md"
+              :class="{ 'bg-green-200': todo.done }"
+            >
+              <div>
+                <div>{{ todo.name }}</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-span-6">
+            <div class="p-8 bg-white rounded shadow-lg">
+              <h2 class="text-xl">Add TODO</h2>
+              <input type="text" class="w-full p-2 mt-4 border rounded" />
+              <button @click="addTodo">Test</button>
+            </div>
           </div>
         </div>
       </div>
-      <button type="button" class="button">
-        Button
-      </button>
     </main>
   </div>
 </template>
@@ -26,8 +40,13 @@ export default defineComponent({
       todoText: '',
     });
 
+    const addTodo = () => {
+      state.todos.push({ name: 'Test', done: false });
+    };
+
     return {
       state,
+      addTodo,
     };
   },
 });
