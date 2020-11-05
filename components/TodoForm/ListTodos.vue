db2398f585d83f6e5c79c5ad4a7d8f6a266ef4b3
greendev8391 <green.dev8391@gmail.com>
2020-11-05 02:24:03 +0100

    Refactor methods into new component

--- components/TodoForm/ListTodos.vue
+++ components/TodoForm/ListTodos.vue
@@ -9,18 +9,68 @@
       <div>
         <div>{{ todo.name }}</div>
       </div>
+      <div class="space-x-2">
+        <button class="btn-error" @click="deleteTodo(index)">Delete</button>
+
+        <button
+          v-if="!todo.done"
+          class="btn-standard"
+          title="Complete TODO"
+          @click="toggleDone(index)"
+        >
+          Complete
+        </button>
+
+        <button
+          v-else
+          class="btn-standard"
+          title="Uncomplete TODO"
+          @click="toggleDone(index)"
+        >
+          Uncomplete
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
   props: {
     todos: { type: Array, default: null },
     name: { type: String, default: '' },
     done: { type: Boolean, default: false },
   },
-}
+  methods: {
+    deleteTodo(index: number) {
+      this.$store.commit('deleteTodo', index)
+    },
+    toggleDone(index: number) {
+      this.$store.commit('toggleDone', index)
+    },
+  },
+})
 </script>
 
-<style></style>
+<style lang="postcss" scoped>
+button {
+  @apply mt-4 px-4 py-2 font-bold text-white rounded;
+}
+.btn-standard {
+  @apply bg-blue-500;
+}
+.btn-error {
+  @apply bg-red-500;
+}
+.btn-standard:hover {
+  @apply bg-blue-700;
+}
+.btn-error:hover {
+  @apply bg-red-700;
+}
+input {
+  @apply w-full p-2 mt-4 border rounded;
+}
+</style>
